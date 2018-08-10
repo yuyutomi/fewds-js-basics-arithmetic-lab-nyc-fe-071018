2477faca925e038af6a07bfe0bfb80d07d14b112
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-10 00:56:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ let newID = oldID + 1000000000
 let ageIsValid = Number.isInteger(currentAge)
 let randomNumber =  Math.random() * 20
 let randomInteger = Math.floor(randomNumber)
+let randomUserId = randomInteger + randomNumber
